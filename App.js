e5b781b2edc1c095fd27ba29471c4828fd99bc29
abrildiazmiguez <adiazm@fi.uba.ar>
2023-08-30 17:26:57 -0300

    Habemus botón

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View, TouchableHighlight } from 'react-native';
 import logo from './our_assets/logo.png'; 
 
 const App = () => {
+  const handleButtonPress = () => {
+    // This function will be called when the button is pressed
+    alert('Button pressed!');
+  };
+
+
+
   return (
     <View style={styles.container}> 
       <Image source={logo} style={styles.logo} />
       <Text style={styles.text}>Welcome to our app!</Text>
+      <TouchableHighlight
+        onPress={handleButtonPress}
+        underlayColor="#a3a5c3" // Define the press color
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Press Me</Text>
+      </TouchableHighlight>
     </View>
   );
 };
@@ -23,6 +36,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#947eb0',
     marginTop: -30,
+  },
+  button: {
+    backgroundColor: '#EDEDF4',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#947EB0',
+    fontWeight: 'bold',
   },
   logo: {
     width: 400,
