9d5d0b9549235bea826424632675aa7ad167ff85
abrildiazmiguez <adiazm@fi.uba.ar>
2023-08-30 16:31:53 -0300

    Uploading expo project. Hello world implemented

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, Expo World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#353839',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#A3A5C3',
+  },
+});
