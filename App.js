b955145b8518e6fa54d513ca3c196724c8671355
abrildiazmiguez <adiazm@fi.uba.ar>
2023-08-30 17:13:31 -0300

    Logo and text is being displayed :D

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, Text, View } from 'react-native';
+import logo from './our_assets/logo.png'; 
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, Expo World!</Text>
+    <View>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.text}>Welcome to our app!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#353839',
-  },
-  text: {
+    justifyContent: 'center', // Center vertically
+    alignItems: 'center', // Center horizontally
+    backgroundColor: '#947EB0',
+  },text: {
     fontSize: 24,
+    justifyContent: 'center', // Center vertically
+    alignItems: 'center', // Center horizontally
     fontWeight: 'bold',
     color: '#947eb0',
+    marginTop: 20,
+  },
+  logo: {
+    width: 400,
+    height: 300,
+    justifyContent: 'center', // Center vertically
+    alignItems: 'center', // Center horizontally
+    resizeMode: 'contain',
   },
 });
+
+export default App;
