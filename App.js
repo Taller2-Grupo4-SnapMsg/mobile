a09d900d3a82aaa6a1568b8aa17ccdf0c83fe6c9
Lara Scazzola <lscazzola@fi.uba.ar>
2023-09-18 22:02:18 -0300

    Probamos algunas cosas mas de navegacion
    Co-authored-by: lara-scazzola1 <lscazzola@fi.uba.ar>
    Co-authored-by: nathencinoza <nencinoza@fi.uba.ar>

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomeScreen from './screens/HomeScreen';
 import SignInScreen from './screens/SignInScreen';
 import WIPScreen from './screens/WIPScreen';
 import SignUpScreen from './screens/SignUpScreen';
@@ -10,16 +9,20 @@ import ExampleScreen from './screens/ExampleScreen';
 import TweetScreen from './screens/TweetScreen';
 import NewTweetScreen from './screens/NewTweetScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import { withLayoutContext } from 'expo-router';
+import {
+  SessionProvider,
+  WithSession,
+  WithoutSession,
+} from './contexts/auth/Auth';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="WIP" component={WIPScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
+    <Stack.Navigator screenOptions={{
+      headerShown: false,
+    }}>
       <Stack.Screen name="Example" component={ExampleScreen} />
       <Stack.Screen name="TweetScreen" component={TweetScreen} />
       <Stack.Screen name="NewTweetScreen" component={NewTweetScreen} />
@@ -28,26 +31,37 @@ const StackNavigator = () => {
   );
 };
 
-const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="SignIn" component={SignInScreen} />
-    </Drawer.Navigator>
-  );
-};
-
-const App = () => {
-  return (
+const App = () => (
+  <SessionProvider>
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="HomeSignIn" component={DrawerNavigator} />
-        <Drawer.Screen name="OtherScreens" component={StackNavigator} />
-        <Drawer.Screen name="Example" component={ExampleScreen} />
-      </Drawer.Navigator>
+    <WithSession>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={StackNavigator} />
+          <Stack.Screen name="WIP (no va)" component={WIPScreen} />
+          <Stack.Screen name="SignUp (no va)" component={SignUpScreen} />
+          <Stack.Screen name="Sign In (no va)" component={SignInScreen} />
+          <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        </Drawer.Navigator>
+    </WithSession>
+    <WithoutSession>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="ExampleScreen" component={MainNavigator} />
+      </Stack.Navigator>
+    </WithoutSession>
     </NavigationContainer>
-  );
-};
+  </SessionProvider>
+);
+
+const MainNavigator = () => (
+  <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Screen name="Home" component={StackNavigator} />
+    {/*<Drawer.Screen name="WIP (no va)" component={WIPScreen} />*/}
+    {/*<Drawer.Screen name="SignUp (no va)" component={SignUpScreen} />*/}
+    {/*<Drawer.Screen name="ProfileScreen" component={ProfileScreen} />*/}
+  </Drawer.Navigator>
+);
 
 export default App;
+
 
