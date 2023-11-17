13257c8543e7b1ca265b25ad0521c485e15d5b1c
Lara Scazzola <lscazzola@fi.uba.ar>
2023-11-16 23:04:37 -0300

    Se acomoda el get de notificaciones, para que sea mas prolijo
    Co-authored-by: nathencinoza <nencinoza@fi.uba.ar>

--- screens/notifications/NotificationsScreen.js
+++ screens/notifications/NotificationsScreen.js
@@ -1,124 +1,107 @@
-//me queda pendiente hacer que traiga de a x, recargue y demas
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, RefreshControl } from 'react-native';
-import { query, orderByChild, onValue, endAt, get, limitToLast, ref, push, serverTimestamp, onChildAdded, off, set } from 'firebase/database';
+import { query, orderByChild, limitToLast, ref, get, onChildAdded, off, endAt } from 'firebase/database';
 import { db } from '../../firebase';
+import { useFocusEffect } from '@react-navigation/native';
 import NotificationMessage from './NotificationMessage';
-import { StyleSheet } from 'react-native';
 import NotificationMention from './NotificationMention';
 import { useUser } from '../../contexts/UserContext';
-import { useRef } from 'react';
-import { useFocusEffect } from '@react-navigation/native';
+import { View, Text, FlatList, RefreshControl, StyleSheet } from 'react-native';
+import LoadingMoreIndicator from '../../components/LoadingMoreIndicator';
+
+const AMOUNT_NOTIFICATIONS = 10;
 
 function generateUserEmailID(user_receiver_email) {
   return `${user_receiver_email.replace(/[\.\#\$\/\[\]]/g, '_')}`;
 }
 
-AMOUNT_NOTIFICATIONS_BACK = 1
-AMOUNT_NOTIFICATIONS_BEGINNING = 1
-
 const NotificationsScreen = () => {
   const { loggedInUser } = useUser();
   const [notifications, setNotifications] = useState([]);
-  const [oldestTimestamp, setOldestTimestamp] = useState(0); // Inicializa con 0
-  const flatListRef = useRef(null);
-  const [refreshing, setRefreshing] = useState(false);
   const notificationsRef = ref(db, `notifications/${generateUserEmailID(loggedInUser.email)}`);
+  const [refreshing, setRefreshing] = useState(false);
+  const [reachedEnd, setReachedEnd] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [fin, setFin] = useState(false);
+  const [oldestTimeStamp, setOldestTimeStamp] = useState(null);
 
-  // ... (resto del código)
-
-  useFocusEffect(
-    React.useCallback(() => {
-      const fetchData = async () => {
-        try {
-          const notificationsRef = ref(db, `notifications/${generateUserEmailID(loggedInUser.email)}`);
-          const notificationQuery = query(
-            notificationsRef,
-            orderByChild('timestamp'),
-            limitToLast(AMOUNT_NOTIFICATIONS_BEGINNING)
-          );
-
-          const snapshot = await get(notificationQuery);
-
-          if (snapshot.exists()) {
-            const notificationsArray = [];
-            snapshot.forEach((childSnapshot) => {
-              const notification = childSnapshot.val();
-              notificationsArray.push(notification);
-            });
-
-            if (notificationsArray.length > 0) {
-              const newNotifications = [...notifications, ...notificationsArray];
-              setNotifications(newNotifications);
-              setOldestTimestamp(notificationsArray[0].timestamp);
-            }
-          }
-        } catch (error) {
-          console.error('Error fetching notifications:', error);
-        }
-      };
-
-      fetchData();
-    }, [loggedInUser.email])
-  );
-
-  const handleGetOlderNotifications = async () => {
-    if (refreshing) return;
-    setRefreshing(true);
-
-    const queryRef = query(
-      notificationsRef,
-      orderByChild('timestamp'),
-      endAt(oldestTimestamp - 1),
-      limitToLast(AMOUNT_NOTIFICATIONS_BACK)
-    );
+  const handleGetMoreNotifications = async (oldestTimeStamp, refresh) => {
+    if (fin) return;
+    if (loadingMore || (reachedEnd && !refresh)) return;
 
     try {
+      setLoadingMore(true);
+      setRefreshing(refresh);
+
+      queryRef = query(
+        notificationsRef,
+        orderByChild('timestamp'),
+        endAt(oldestTimeStamp - 1), 
+        limitToLast(AMOUNT_NOTIFICATIONS)
+      );
+
       const snapshot = await get(queryRef);
 
       if (snapshot.exists()) {
-        const notificationsArray = [];
-        snapshot.forEach((childSnapshot) => {
-          const notification = childSnapshot.val();
-          notificationsArray.push(notification);
-        });
-
+        const notificationsArray = Object.values(snapshot.val()).reverse();
         if (notificationsArray.length > 0) {
-          const newNotifications = [...notifications, ...notificationsArray];
-          setNotifications(newNotifications);
-          setOldestTimestamp(notificationsArray[0].timestamp);
+          if (refresh) {
+            setNotifications(notificationsArray);
+            setRefreshing(false);
+            setReachedEnd(false);
+          } else {
+            setNotifications((prevNotifications) => [...prevNotifications, ...notificationsArray]);
+          }
+          if (notificationsArray.length >= AMOUNT_NOTIFICATIONS) {
+            const newOldestTimestamp = notificationsArray[AMOUNT_NOTIFICATIONS - 1].timestamp;
+            setOldestTimeStamp(newOldestTimestamp);
+          } else {
+            setFin(true);
+          }
+        } else {
+          setReachedEnd(true);
+          setRefreshing(false);
         }
       }
-
-      setRefreshing(false);
     } catch (error) {
-      console.error('Error fetching earlier notifications:', error);
+      console.error('Error while loading more notifications:', error);
+    } finally {
+      setLoadingMore(false);
     }
   };
 
+  useFocusEffect(
+    React.useCallback(() => {
+      handleGetMoreNotifications((new Date()).getTime(), true);
+    }, [])
+  );
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Notifications</Text>
       <FlatList
-        ref={flatListRef}
         data={notifications}
-
-        renderItem={({ item }) => {
-          if (item.type === 'message') {
-            return <NotificationMessage message={item.body} data={item.data} read={item.read}  />;
-          } else if (item.type === 'mention') {
-            return <NotificationMention message={item.body} data={item.data} read={item.read}  />;
-          }
-        }}
         keyExtractor={(item) => item.notificationId}
-        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        renderItem={({ item }) => (
+          <View style={styles.notificationContainer}>
+            {item.type === 'message' ? (
+              <NotificationMessage message={item.body} data={item.data} read={item.read} />
+            ) : item.type === 'mention' ? (
+              <NotificationMention message={item.body} data={item.data} read={item.read} />
+            ) : null}
+            <View style={styles.separator} />
+          </View>
+        )}
         refreshControl={
           <RefreshControl
             refreshing={refreshing}
-            onRefresh={handleGetOlderNotifications}
+            onRefresh={() => handleGetMoreNotifications((new Date()).getTime(), true)}
+            colors={['#947EB0']}
           />
         }
+        onEndReached={() => handleGetMoreNotifications(oldestTimeStamp, false)}
+        onEndReachedThreshold={0.1}
       />
+    {loadingMore && <LoadingMoreIndicator />}
     </View>
   );
 };
